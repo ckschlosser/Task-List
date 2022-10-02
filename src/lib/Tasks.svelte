2ae0aa28a5664748891ab1a9e513a3942bb9fd67
Clinton <clintonschlosser@Clinton.local>
2022-10-02 12:01:53 -0500

    Refractor functions

--- src/lib/Tasks.svelte
+++ src/lib/Tasks.svelte
@@ -7,16 +7,17 @@
     let task_text;
     let currentlyEditing = false;
     $:tasks = [];
-
+    let dataUpdate = async function() {
+      await supabase
+      .from('Tasks')
+      .select('*')
+      tasks = this.data;
+    }
 
     // Get tasks from supabase
     onMount(async () => {
 		
-        let data = await supabase
-            .from('Tasks')
-            .select('*')
-
-        tasks = data.data
+        dataUpdate();
         console.log('Tasks:', tasks);
 
 	});
@@ -33,11 +34,7 @@
         if (error) {
             console.log('Error adding task: ', error);
         } else {
-            let afterAddData = await supabase
-              .from('Tasks')
-              .select('*')
-
-            tasks = afterAddData.data;
+            dataUpdate();
             task_text = '';            
 
         }
@@ -64,11 +61,7 @@
       if (error) {
             console.log('Error editing task: ', error);
         } else {
-            let afterEditData = await supabase
-              .from('Tasks')
-              .select('*')
-
-            tasks = afterEditData.data;
+            dataUpdate();
             currentlyEditing = false;
         }
         
@@ -88,92 +81,12 @@
       if (error) {
             console.log('Error deleting task: ', error);
         } else {
-            let afterDeleteData = await supabase
-              .from('Tasks')
-              .select('*')
-
-            tasks = afterDeleteData.data
+            dataUpdate();
 
 
         }
 
     }
-
-   /* window.addEventListener("load", () => {
-  const form = document.querySelector("#new-task-form");
-  const input = document.querySelector("#new-task-input");
-  const list_el = document.querySelector("#tasks");
-
-  form.addEventListener("submit", (e) => {
-    
-    console.log('I Added one');
-    e.preventDefault();
-
-    const task = input.value;
-    if (!task) {
-      alert("Please fill out the task");
-      return;
-    }
-
-    const task_el = document.createElement("div");
-    task_el.classList.add("task");
-
-    const task_content_el = document.createElement("div");
-    task_content_el.classList.add("content");
-
-    task_el.appendChild(task_content_el);
-
-    // Input
-    const task_input_el = document.createElement("input");
-
-    // Add
-    task_input_el.classList.add("text");
-    task_input_el.type = "text";
-    task_input_el.value = task;
-    task_input_el.setAttribute("readonly", "readonly");
-    task_content_el.appendChild(task_input_el);
-    const task_actions_el = document.createElement("div");
-
-    task_actions_el.classList.add("actions");
-
-    // Edit
-    const task_edit_el = document.createElement("button");
-    task_edit_el.classList.add("edit");
-    task_edit_el.innerHTML = "Edit";
-
-    // Delete
-    const task_delete_el = document.createElement("button");
-    task_delete_el.classList.add("delete");
-    task_delete_el.innerHTML = "Delete";
-
-    task_actions_el.appendChild(task_edit_el);
-    task_actions_el.appendChild(task_delete_el);
-
-    task_el.appendChild(task_actions_el);
-
-    // Clicked Add
-    list_el.appendChild(task_el);
-
-    input.value = "";
-
-    task_edit_el.addEventListener("click", () => {
-      if (task_edit_el.innerText.toLowerCase() == "edit") {
-        console.log('I Edited one');
-        task_input_el.removeAttribute("readonly");
-        task_input_el.focus();
-        task_edit_el.innerText = "Save";
-      } else {
-        console.log('I Saved one');
-        task_input_el.setAttribute("readonly", "readonly");
-        task_edit_el.innerText = "Edit";
-      }
-    });
-
-    task_delete_el.addEventListener("click", () => {
-      list_el.removeChild(task_el);
-    });
-  });
-});*/
 </script>
 
     
